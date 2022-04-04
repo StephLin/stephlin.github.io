784bc91c5a91a655acda07d30700b11cfa07d6b1
StephLin <StephLin@users.noreply.github.com>
2022-04-04 12:58:03 +0000

    deploy: 5f1eee242631c0a4e990ccafd87cddf4708ad330

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,7 +1,7 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
     VERSION: '',
-    LANGUAGE: 'zh',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'dirhtml',
     FILE_SUFFIX: '.html',
